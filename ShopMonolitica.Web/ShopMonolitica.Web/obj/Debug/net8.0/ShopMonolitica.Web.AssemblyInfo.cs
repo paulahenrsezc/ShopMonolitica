0068db760a715382454015e50bd3540d45754622
sebastian escaño perez <prezs692@gmail.com>
2024-05-26 15:17:58 -0400

    Conclusión de la arquitectura con las entidades x y z agregada.

--- ShopMonolitica.Web/ShopMonolitica.Web/obj/Debug/net8.0/ShopMonolitica.Web.AssemblyInfo.cs
+++ ShopMonolitica.Web/ShopMonolitica.Web/obj/Debug/net8.0/ShopMonolitica.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ShopMonolitica.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc750e97d3c00b773590c7967bf8c67613948142")]
 [assembly: System.Reflection.AssemblyProductAttribute("ShopMonolitica.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ShopMonolitica.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
