424a55dac9f517e92bff7e70d9f7275437e318b5
EzeQuezada <20231021.itla.edu.do>
2024-05-28 12:01:56 -0400

    Prueba uno

---
+++ ShopMonolitica.Web/ShopMonolitica.Web/Data/DbObjects/ProductsDb.cs
@@ -0,0 +1,18 @@
+﻿using ShopMonolitica.Web.Data.Core;
+
+namespace ShopMonolitica.Web.Data.DbObjects
+{
+    public class ProductsDb: BaseEntity
+    {
+        #region
+        public string ProductName { get; set; }
+        public string ProductDescription { get; set; }
+        public string ProductCategory { get; set; }
+        public string ProductType { get; set; }
+
+        = string.Empty;
+        public ProductsDb() { }
+        #endregion  
+
+    }
+}
