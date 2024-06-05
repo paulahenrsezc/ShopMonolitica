142945c79a736304c5669105721aef3e803e7bde
SEP112015 <154470382+SEP112015@users.noreply.github.com>
2024-06-04 22:02:23 -0400

    Creación del Exception

--- ShopMonolitica.Web/ShopMonolitica.Web/Data/DbObjects/CustomersDb.cs
+++ ShopMonolitica.Web/ShopMonolitica.Web/Data/DbObjects/CustomersDb.cs
@@ -1,10 +1,11 @@
 ﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using ShopMonolitica.Web.Data.Context;
 using ShopMonolitica.Web.Data.Entities;
+using ShopMonolitica.Web.Data.Extentions;
 using ShopMonolitica.Web.Data.interfaces;
 using ShopMonolitica.Web.Data.Models;
 using System.Runtime.Intrinsics.Arm;
-
+using ShopMonolitica.Web.Data.Extentions;
 namespace ShopMonolitica.Web.Data.DbObjects
 {
     public class CustomersDb : ICustomersDb
@@ -18,41 +19,14 @@ namespace ShopMonolitica.Web.Data.DbObjects
 
         public CustomersModel GetCustomers(int custid)
         {
-            var customers = _shopContext.Customers.Find(custid);
-
-            CustomersModel customersModel = new CustomersModel()
-            {
-                companyname = customers.companyname,
-                contactname = customers.contactname,
-                contacttitle = customers.contacttitle,
-                address = customers.address,
-                email = customers.email,
-                city = customers.city,
-                region = customers.region,
-                postalcode = customers.postalcode,
-                country = customers.country,
-                phone = customers.phone,
-                fax = customers.fax
-            };
-
-            return customersModel;
-            }
+            var customers = _shopContext.Customers.Find(custid).ConvertCustEntityToCustomersModel();
+            return customers;
+        }
         public List<CustomersModel> GetCustomers()
         {
-            return _shopContext.Customers.Select(customers => new CustomersModel() 
-            {
-                companyname = customers.companyname,
-                contactname = customers.contactname,
-                contacttitle = customers.contacttitle,
-                address = customers.address,
-                email = customers.email,
-                city = customers.city,
-                region = customers.region,
-                postalcode = customers.postalcode,
-                country = customers.country,
-                phone = customers.phone,
-                fax = customers.fax
-            }).ToList();
+            return _shopContext.Customers
+                .Select(customers => customers.ConvertCustEntityToCustomersModel())
+                .ToList();
         }
 
         public void RemoveCustomer()
@@ -62,40 +36,21 @@ namespace ShopMonolitica.Web.Data.DbObjects
 
         public void SaveCustomers(CustomersSaveModel customersSave)
         {
-            Customers customers = new Customers()
-            {
-                companyname = customersSave.companyname,
-                contactname = customersSave.contactname,
-                contacttitle = customersSave.contacttitle,
-                address = customersSave.address,
-                email = customersSave.email,
-                city = customersSave.city,
-                region = customersSave.region,
-                postalcode = customersSave.postalcode,
-                country = customersSave.country,
-                phone = customersSave.phone,
-                fax = customersSave.fax
-            };
-            _shopContext.Customers.Add(customers);
-            _shopContext.SaveChanges();
+            Customers customerEntity = customersSave.ConvertCustomersSaveModelToCustomersEntity();
+            _shopContext.Customers.Add(customerEntity);
+            _shopContext.SaveChanges(); ;
         }
 
         public void UpdateCustomes(CustomerUpdateModel updateModel)
         {
-            Customers customersUpdate = _shopContext.Customers.Find(updateModel.custid);
-            customersUpdate.companyname = updateModel.companyname;
-            customersUpdate.contactname = updateModel.contactname;
-            customersUpdate.contacttitle = updateModel.contacttitle;
-            customersUpdate.address = updateModel.address;
-            customersUpdate.email = updateModel.email;
-            customersUpdate.city = updateModel.city;
-            customersUpdate.region = updateModel.region;
-            customersUpdate.postalcode = updateModel.postalcode;
-            customersUpdate.country = updateModel.country;
-            customersUpdate.phone = updateModel.phone;
-            customersUpdate.fax = updateModel.companyname;
-            _shopContext.Customers.Update(customersUpdate);
-            _shopContext.SaveChanges();
+            Customers customersToUpdate = _shopContext.Customers.Find(updateModel.custid);
+
+            if (customersToUpdate != null)
+            {
+                customersToUpdate.UpdateFromModel(updateModel);
+                _shopContext.Customers.Update(customersToUpdate);
+                _shopContext.SaveChanges();
+            }
         }
     }
 }
--- ShopMonolitica.Web/ShopMonolitica.Web/Data/DbObjects/UsersDb.cs
+++ ShopMonolitica.Web/ShopMonolitica.Web/Data/DbObjects/UsersDb.cs
@@ -1,8 +1,9 @@
 ﻿using ShopMonolitica.Web.Data.Context;
 using ShopMonolitica.Web.Data.Entities;
+using ShopMonolitica.Web.Data.Extentions;
 using ShopMonolitica.Web.Data.interfaces;
 using ShopMonolitica.Web.Data.Models;
-
+using ShopMonolitica.Web.Data.Extentions;
 namespace ShopMonolitica.Web.Data.DbObjects
 {
     public class UsersDb : IUsersDb
@@ -16,52 +17,39 @@ namespace ShopMonolitica.Web.Data.DbObjects
 
         public UsersModel GetUsers(int UserId)
         {
-            var users = _shopContext.Users.Find(UserId);
-
-            UsersModel usersModel = new UsersModel()
-            {
-                Email = users.Email,
-                Password = users.Password,
-                Name = users.Name
-            };
-            return usersModel;
+            var user = _shopContext.Users.Find(UserId).ConvertUsersEntityToUsersModel();
+            return user;
         }
 
         public List<UsersModel> GetUsers()
         {
-            return _shopContext.Users.Select(users => new UsersModel()
-            {
-                Email = users.Email,
-                Password = users.Password,
-                Name = users.Name
-            }).ToList();
+            return _shopContext.Users
+                .Select(user => user.ConvertUsersEntityToUsersModel())
+                .ToList();
         }
 
         public void RemoveUser()
         {
-            Users userDelete = _shopContext.Users.Find();
+            throw new NotImplementedException();
         }
 
         public void SaveUser(UsersSaveModel usersSave)
         {
-            Users users = new Users()
-            {
-                Email = usersSave.Email,
-                Password = usersSave.Password,
-                Name = usersSave.Name
-            };
-            _shopContext.Users.Add(users);
+            Users userEntity = usersSave.ConvertUsersSaveModelToUsersEntity();
+            _shopContext.Users.Add(userEntity);
             _shopContext.SaveChanges();
         }
 
         public void UpdateUser(UsersUpdateModel usersModel)
         {
-            Users usersUpdate = _shopContext.Users.Find(usersModel.UserId);
-            usersUpdate.Email = usersModel.Email;
-            usersUpdate.Password = usersModel.Password;
-            usersUpdate.Name = usersModel.Name;
-            _shopContext.Users.Update(usersUpdate);
-            _shopContext.SaveChanges();
+            Users userToUpdate = _shopContext.Users.Find(usersModel.UserId);
+
+            if (userToUpdate != null)
+            {
+                userToUpdate.UpdateFromModel(usersModel); 
+                _shopContext.Users.Update(userToUpdate);
+                _shopContext.SaveChanges();
+            }
         }
     }
 }
